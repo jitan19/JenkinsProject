12b7621a0d8e247b17021d9d141006af546d1011
jitan19 <61877117+jitan19@users.noreply.github.com>
2020-03-07 03:42:12 +0530

    Update HelloWorld.java

--- src/com/amarshi/helloworld/jenkins/HelloWorld.java
+++ src/com/amarshi/helloworld/jenkins/HelloWorld.java
@@ -3,7 +3,7 @@ package com.amarshi.helloworld.jenkins;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("--------This is my first Hello World Jenkins Project-------------");
+		System.out.println("--------Modified content-------------");
 
 	}
 
