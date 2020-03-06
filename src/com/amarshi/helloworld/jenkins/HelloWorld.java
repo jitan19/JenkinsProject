1a5446261c18a6d6f79625cb08ae56d98ac20a4a
Amarshi Mohanty <amarshimohanty@gmail.com>
2020-03-07 03:28:37 +0530

    Initial Commit

---
+++ src/com/amarshi/helloworld/jenkins/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.amarshi.helloworld.jenkins;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("--------This is my first Hello World Jenkins Project-------------");
+
+	}
+
+}
